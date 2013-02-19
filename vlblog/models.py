2e369dab2f1d816248a92cbf69cd29137f1ed620
Vyacheslav Levit <levit.slava@gmail.com>
2013-02-19 23:51:56 +0200

    Add basic models

--- vlblog/models.py
+++ vlblog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    blog = models.ForeignKey(Blog)
+    created = models.DateTimeField()
+    title = models.CharField(max_length=200, blank=True)
+    tags = models.ManyToManyField(Tag)
+    body = models.TextField()
+    excerpt = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.title
