641c228d6f7f2202449fe134ace32ba0d11b9c48
Vyacheslav Levit <levit.slava@gmail.com>
2013-03-03 18:43:44 +0200

    Make logging less verbose

--- vlevitorg/settings.py
+++ vlevitorg/settings.py
@@ -134,7 +134,7 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s %(asctime)s %(module)s,%(lineno)d: %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
