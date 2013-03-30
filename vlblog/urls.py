4dd23557a15372ba4770e46a07d8810c8d3c2fa9
Vyacheslav Levit <levit.slava@gmail.com>
2013-03-30 20:09:49 +0200

    Fix template links

--- vlblog/urls.py
+++ vlblog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('vlblog.views.posts',
     url(r'^blog/(?P<blog>[\w-]+)/?$', 'post_list', name='post_list'),
-    url(r'^blog/(?P<blog>[\w-]+)/tag/(?P<tag>.*)', 'post_list', name='post_list_tag'),
-    url(r'^blog/(?P<blog>[\w-]+)/(?P<post>[\w-]+)', 'post'),
+    url(r'^blog/(?P<blog>[\w-]+)/tag/(?P<tag>.*)', 'post_list',
+        name='post_list_tag'),
+    url(r'^blog/(?P<blog>[\w-]+)/(?P<post>[\w-]+)', 'post', name='post'),
 )
