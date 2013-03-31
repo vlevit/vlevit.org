28b87ee5a1e1ed40c10b5b8054992387a2b67028
Vyacheslav Levit <levit.slava@gmail.com>
2013-03-31 20:54:40 +0300

    Unnecessary /? in urls.py regexps

--- vlblog/urls.py
+++ vlblog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('vlblog.views.posts',
-    url(r'^blog/(?P<blog>[\w-]+)/?$', 'post_list', name='post_list'),
+    url(r'^page/(?P<page>[\w-]+)/$', 'page', name='page'),
+    url(r'^blog/(?P<blog>[\w-]+)/$', 'post_list', name='post_list'),
     url(r'^blog/(?P<blog>[\w-]+)/tag/(?P<tag>.*)', 'post_list',
         name='post_list_tag'),
     url(r'^blog/(?P<blog>[\w-]+)/(?P<post>[\w-]+)', 'post', name='post'),
