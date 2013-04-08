673fb321bcb4068c3a25824b47b4e591f3e91184
Vyacheslav Levit <levit.slava@gmail.com>
2013-04-08 23:52:21 +0300

    Enable Django Debug Toolbar in settings

--- vlevitorg/settings.py
+++ vlevitorg/settings.py
@@ -104,9 +104,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = 'vlevitorg.urls'
 
@@ -129,14 +130,27 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    # 'django.contrib.admindocs',
     'threadedcomments',
     'django.contrib.comments',
     'vlblog',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'debug_toolbar',
 )
 
 COMMENTS_APP = 'threadedcomments'
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
 
 LOGGING = {
     'version': 1,
