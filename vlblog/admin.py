e4a30d0fb39b1ede70237b329248611004bad740
Vyacheslav Levit <levit.slava@gmail.com>
2013-06-19 19:40:38 +0300

    Add very basic admin interface for vlblog

---
+++ vlblog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from vlblog.models import Blog, File, Page, Post, Tag
+
+
+admin.site.register(Blog)
+admin.site.register(File)
+admin.site.register(Page)
+admin.site.register(Post)
+admin.site.register(Tag)
