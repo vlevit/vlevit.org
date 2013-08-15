689070c7239383356c8fcd32cf437a641de86fd2
Vyacheslav Levit <levit.slava@gmail.com>
2013-08-16 00:22:22 +0300

    Disable SetRemoteAddrFromForwardedFor middleware

--- vlevitorg/settings.py
+++ vlevitorg/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'middleware.http.SetRemoteAddrFromForwardedFor',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
