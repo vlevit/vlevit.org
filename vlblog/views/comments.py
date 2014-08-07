df10fea2f26d0bb1bc733a85878225f56c267ea5
Vyacheslav Levit <levit.slava@gmail.com>
2014-08-07 16:16:32 +0300

    Comments import and export for pages

--- vlblog/views/comments.py
+++ vlblog/views/comments.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 import logging
 import os
-from os import path
+import os.path
 
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
@@ -22,15 +22,16 @@ logger = logging.getLogger(__name__)
 
 
 @require_key
-def import_comments(request, blog_dir=settings.BLOG_DIR):
+def import_comments(request):
     """
-    Import all comments found in [blog_dir]/[blog]/comments to the database.
+    Import all comments to the database from the following locations:
+        <folder>/blog/**/comments
+        <folder>/pages/**/comments
 
-    Name of comment file is excepted to be the same (except extension)
-    as for post file.
+    Comment file names are excepted to be the same as post/page names
+    (excluding extension).
 
     """
-
     blog_info_cache = {}
     conf_loader = BlogConfLoader()
 
@@ -38,29 +39,88 @@ def import_comments(request, blog_dir=settings.BLOG_DIR):
     yaml.add_constructor(u'tag:yaml.org,2002:timestamp',
                          lambda cls, node: parse_datetime(node.value))
 
-    for root, dirs, files in os.walk(blog_dir):
-
-        # we are interested only in comments directories
-        if not root.endswith('/comments'):
-            continue
-        root_parent = path.dirname(root)
-
+    def read_blog_info(folder):
         # read blog configuration
-        blog_conf = path.join(root_parent, 'blog.conf')
-        if not path.exists(blog_conf):
-            logger.info("no blog.conf in %s, directory skipped", root_parent)
-            continue
-        if root_parent in blog_info_cache:
-            blog_info = blog_info_cache['root_parent']
+        blog_info = None
+        blog_conf = os.path.join(folder, 'blog.conf')
+        if folder in blog_info_cache:
+            blog_info = blog_info_cache[folder]
+        elif not os.path.exists(blog_conf):
+            logger.info("no blog.conf in %s, directory skipped", folder)
         else:
             try:
                 blog_info = conf_loader.load(blog_conf)
             except ConfLoaderError, e:
                 logger.error(unicode(e))
                 logger.info("directory %s skipped", root)
+                return None
+            else:
+                blog_info_cache[folder] = blog_info
+        return blog_info
+
+    def load_comments(path):
+        logger.info('loading %s', path)
+        comments = None
+        with open(path) as f:
+            try:
+                comments = yaml.safe_load(f)
+            except yaml.YAMLError, e:
+                logger.error("error loading a comment file: %s", e)
+        return comments
+
+    def extract_language_from_path(path):
+        language = None
+        start = path.find('/pages/')
+        if start != -1:
+            start += 7
+            end = path.find('/', start)
+            if end != -1:
+                language = path[start:end]
+        return language
+
+    def import_comments_list(comments, obj, parent=None):
+        # import comments to a database
+        for c in comments:
+            # consider comments with equal date to be the same
+            # this makes possible to update all other comment fields
+            try:
+                comment = ThreadedComment.objects.get(
+                    submit_date=c['published'])
+            except ThreadedComment.DoesNotExist:
+                comment_pk = None
+            except ThreadedComment.MultipleObjectsReturned:
+                logger.error("more then one comment with the same "
+                             "publish date: %s", c['published'])
                 continue
             else:
-                blog_info_cache[root_parent] = blog_info
+                comment_pk = comment.pk
+            comment = ThreadedComment(
+                pk=comment_pk,
+                content_type=ContentType.objects.get_for_model(
+                    obj.__class__),
+                object_pk=unicode(obj.pk),
+                user_name=c['author'],
+                user_url=c.get('website', ''),
+                comment=c['content'],
+                submit_date=c['published'],
+                site_id=settings.SITE_ID,
+                is_public=True,
+                is_removed=False,
+                parent=parent,
+                comment_html=safe_markdown(c['content']))
+            comment.save()
+            if 'replies' in c:
+                import_comments_list(c['replies'], obj, comment)
+
+    for root, dirs, files in os.walk(settings.BLOG_DIR):
+
+        # we are interested only in comments directories
+        if not root.endswith('/comments'):
+            continue
+
+        blog_info = read_blog_info(os.path.dirname(root))
+        if not blog_info:
+            continue
 
         # get Blog model object
         try:
@@ -71,72 +131,61 @@ def import_comments(request, blog_dir=settings.BLOG_DIR):
             continue
 
         # iterate through all comment files in the directory and import them to
-        # a database
+        # the database
         for comment_file in files:
             if not comment_file.endswith('.yaml'):
                 continue
-            comment_file = path.join(root, comment_file)
-            post_name = utils.name_from_file(comment_file)
+            comment_file = os.path.join(root, comment_file)
+            name = utils.name_from_file(comment_file)
             try:
-                post = models.Post.objects.get(blog=blog, name=post_name)
+                post = models.Post.objects.get(blog=blog, name=name)
             except models.Post.DoesNotExist:
-                logger.error("post '%s' doesn't exist", post_name)
+                logger.error("post '%s' doesn't exist", name)
                 continue
 
-            # load comment file
-            with open(comment_file) as f:
-                try:
-                    comments = yaml.safe_load(f)
-                except yaml.YAMLError, e:
-                    logger.error("error loading a comment file: %s", e)
-                    continue
+            comments = load_comments(comment_file)
+            import_comments_list(comments, post)
 
-            def import_comment_list(comments, parent=None):
-                # import comments to a database
-                for c in comments:
-                    # consider comments with equal date to be the same
-                    # this makes possible to update all other comment fields
-                    try:
-                        comment = ThreadedComment.objects.get(
-                            submit_date=c['published'])
-                    except ThreadedComment.DoesNotExist:
-                        comment_pk = None
-                    except ThreadedComment.MultipleObjectsReturned:
-                        logger.error("more then one comment with the same "
-                                     "publish date: %s", c['published'])
-                        continue
-                    else:
-                        comment_pk = comment.pk
-                    comment = ThreadedComment(
-                        pk=comment_pk,
-                        content_type=ContentType.objects.get_for_model(
-                            models.Post),
-                        object_pk=unicode(post.id),
-                        user_name=c['author'],
-                        user_url=c.get('website', ''),
-                        comment=c['content'],
-                        submit_date=c['published'],
-                        site_id=settings.SITE_ID,
-                        is_public=True,
-                        is_removed=False,
-                        parent=parent,
-                        comment_html=safe_markdown(c['content']))
-                    comment.save()
-                    if 'replies' in c:
-                        import_comment_list(c['replies'], comment)
+    for root, dirs, files in os.walk(settings.PAGES_DIR):
 
-            import_comment_list(comments)
+        if not root.endswith('/comments'):
+            continue
+
+        language = extract_language_from_path(root)
+        if not language:
+            logger.error("couldn't extract language from path %s", root)
+            continue
+
+        for comment_file in files:
+            if not comment_file.endswith('.yaml'):
+                continue
+            comment_file = os.path.join(root, comment_file)
+            name = utils.name_from_file(comment_file)
+            try:
+                page = models.Page.objects.get(language=language, name=name)
+            except models.Page.DoesNotExist:
+                logger.error("page '%s' (%s) doesn't exist", name, language)
+                continue
+
+            comments = load_comments(comment_file)
+            import_comments_list(comments, page)
 
     return HttpResponse('<html><body>See log</body></html>')
 
 
 @require_key
-def export_comments(request, dir='comments'):
+def export_comments(request, folder='comments'):
     """
-    Export all Post comments in database to readable yaml files.
+    Export all post and page comments from the database to readable yaml files.
 
-    One yaml file is created per post with comments and is put under
-    [dir]/[language]/[blog]/comments directory in the default storage.
+    For every post and page with comments a single YAML file is created
+    in the default storage.
+
+    The path for post comments file is the following:
+    [folder]/blog/[language]/[blog]/comments/[post].yaml
+
+    The path for page comments file:
+    [folder]/pages/[language]/comments/[page].yaml
 
     """
 
@@ -160,7 +209,7 @@ def export_comments(request, dir='comments'):
     yaml.add_representer(OrderedDict,
                          lambda dumper, d: dumper.represent_dict(d.items()))
 
-    def make_comment_dict(comment):
+    def get_comment_dict(comment):
         comment_dict = OrderedDict()
         comment_dict['author'] = plain(comment.user_name)
         if comment.user_url:
@@ -168,6 +217,42 @@ def export_comments(request, dir='comments'):
         comment_dict['published'] = comment.submit_date
         comment_dict['content'] = literal(comment.comment)
         return comment_dict
+
+    def get_comments_tree(comments):
+        comments_tree = []      # hierarchical representation of comment dicts
+        comments_map = {}       # plain mapping between id and comment itself
+
+        for comment in comments:
+            comment_dict = get_comment_dict(comment)
+            if comment.parent_id not in comments_map:
+                comments_tree.append(comment_dict)
+            else:
+                parent_dict = comments_map[comment.parent_id]
+                if 'replies' not in parent_dict:
+                    parent_dict['replies'] = []
+                parent_dict['replies'].append(comment_dict)
+            comments_map[comment.id] = comment_dict
+        return comments_tree
+
+    def get_post_path(post):
+        return os.path.join(folder, 'blog', post.blog.language, post.blog.name,
+                            "comments", post.name + '.yaml')
+
+    def get_page_path(page):
+        return os.path.join(folder, 'pages', page.language, page.name,
+                            "comments", page.name + '.yaml')
+
+    def dump_yaml(path, comments_tree):
+        logger.info('writing %s', path)
+        try:
+            file = ContentFile(path)
+            yaml.dump(comments_tree, file,
+                      encoding='utf-8', width=80,
+                      indent=4, default_flow_style=False,
+                      allow_unicode=True)
+            path = default_storage.save(path, file)
+        except (IOError, OSError) as err:
+            logger.error('error creating file %s: %s', path, err)
 
     for post in models.Post.objects.all():
 
@@ -177,32 +262,22 @@ def export_comments(request, dir='comments'):
             is_public=True,
             is_removed=False)
 
-        comment_list = []       # hierarchical representation of comment dicts
-        comment_ref = {}        # plain mapping between id and comment itself
+        comments_tree = get_comments_tree(comments)
+        if comments_tree:
+            path = get_post_path(post)
+            dump_yaml(path, comments_tree)
 
-        for comment in comments:
-            comment_dict = make_comment_dict(comment)
-            if comment.parent_id not in comment_ref:
-                comment_list.append(comment_dict)
-            else:
-                parent_dict = comment_ref[comment.parent_id]
-                if 'replies' not in parent_dict:
-                    parent_dict['replies'] = []
-                parent_dict['replies'].append(comment_dict)
-            comment_ref[comment.id] = comment_dict
+    for page in models.Page.objects.all():
 
-        if comment_list:
-            file_path = path.join(dir, post.blog.language,
-                                  post.blog.name, "comments",
-                                  post.name + '.yaml')
-            try:
-                file = ContentFile(file_path)
-                yaml.dump(comment_list, file,
-                          encoding='utf-8', width=80,
-                          indent=4, default_flow_style=False,
-                          allow_unicode=True)
-                file_path = default_storage.save(file_path, file)
-            except (IOError, OSError) as err:
-                logger.error('error creating file %s: %s', file_path, err)
+        comments = ThreadedComment.objects.filter(
+            content_type=ContentType.objects.get_for_model(models.Page),
+            object_pk=page.pk,
+            is_public=True,
+            is_removed=False)
+
+        comments_tree = get_comments_tree(comments)
+        if comments_tree:
+            path = get_page_path(page)
+            dump_yaml(path, comments_tree)
 
     return HttpResponse('<html><body>See log</body></html>')
